b626ef397b5f193fb24216351a0cd02999245b3b
Prateek kumar singh <113665258+Prateek-singh-06@users.noreply.github.com>
2026-03-20 00:11:43 +0530

    Time: 10 ms (78.23%) | Memory: 19.3 MB (89.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s1={}
-        t1={}
+        s_set={}
         for item in s:
-            s1[item]=s1.get(item,0)+1
+            s_set[item]=s_set.get(item,0)+1
         for item in t:
-            t1[item]=t1.get(item,0)+1
-        for key, value in s1.items():
-            if(t1.get(key,0)!=value):
-                return False
-        for key, value in t1.items():
-            if(s1.get(key,0)!=value):
+            s_set[item]=s_set.get(item,0)-1
+        for key, value in s_set.items():
+            if(value!=0):
                 return False
         return True
 
