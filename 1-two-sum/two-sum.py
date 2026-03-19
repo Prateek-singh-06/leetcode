6cf0c84e9754eed54702a50cebcf2cbb4a9126ae
Prateek kumar singh <113665258+Prateek-singh-06@users.noreply.github.com>
2026-03-20 00:33:42 +0530

    Time: 2783 ms (5.54%) | Memory: 19.7 MB (98.36%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,10 +4,10 @@ class Solution:
             new_target=target-num1
             for j,num2 in enumerate(nums):
                 if(j!=i and num2==new_target):
-                    ans=[]
-                    ans.append(i)
-                    ans.append(j)
+                    ans=[i,j]
+                    # ans.append(i)
+                    # ans.append(j)
                     return ans
             # print(i)
-        return [1,2]
+        # return [1,2]
         
