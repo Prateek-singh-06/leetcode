f068efc3d241d9b95bec6244874a1809afa9a87c
Prateek kumar singh <113665258+Prateek-singh-06@users.noreply.github.com>
2026-03-20 14:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (95.90%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        majoritylen=len(nums)//2
-        majlen=0
-        maj=0
-        frequency={}
-        for i,value in enumerate(nums):
-            frequency[value]=frequency.get(value,0)+1
-            if(frequency[value]>majoritylen):
-                majlen=frequency[value]
-                maj=value
+        maj=None
+        count=0
+        for num in nums:
+            if(count==0):
+                maj=num
+                # coutn+=1
+            if(maj==num):
+                count+=1
+            else:
+                count-=1
         return maj
+        # majoritylen=len(nums)//2
+        # majlen=0
+        # maj=0
+        # frequency={}
+        # for i,value in enumerate(nums):
+        #     frequency[value]=frequency.get(value,0)+1
+        #     if(frequency[value]>majoritylen):
+        #         majlen=frequency[value]
+        #         maj=value
+        # return maj
 
 
         # print (majoritylen)
